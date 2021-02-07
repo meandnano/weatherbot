04b575a8ceb7695af9a4ad45ac8833ed11dc6778
meandnano <meandnano@gmail.com>
2021-02-07 22:53:03 +0100

    Migrate to defaultenv

---
+++ env_support.py
@@ -0,0 +1,20 @@
+from typing import TypeVar, Optional
+
+VAR = TypeVar('VAR')
+
+
+class VarNotProvidedException(Exception):
+    env_var: str
+
+    def __init__(self, env_var: str):
+        self.env_var = env_var
+
+    def __str__(self):
+        return f"Env var {self.env_var} is not provided. Could also be served trough .env file"
+
+
+def require_env(var: Optional[VAR], var_name: str) -> VAR:
+    if var is None:
+        raise VarNotProvidedException(var_name)
+
+    return var
--- fetcher.py
+++ fetcher.py
@@ -1,46 +1,18 @@
-import os
 import sys
 from signal import signal, SIGQUIT, SIGINT, SIGTERM
 from time import sleep
-from typing import Any, Optional
 
 import requests
+from defaultenv import env
 from redis import Redis
 
 import weather
+from env_support import require_env
 
 CITY_ID_STHLM = 2673722
 CITY_ID_SOLNA = 2675397
 CITY_CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?" \
                            "id={id}&appid={key}&units=metric"
-
-
-class ArgNotProvidedException(Exception):
-    cli_idx: int
-    env_var: str
-
-    def __init__(self, cli_idx: int, env_var: str):
-        self.cli_idx = cli_idx
-        self.env_var = env_var
-
-    def __str__(self):
-        return f"Arg at index {self.cli_idx} is not provided. Could be env var {self.env_var}"
-
-
-def read_arg(cli_idx: int, env_name: str, default: Optional[Any] = None) -> str:
-    value: Any
-    if len(sys.argv) > cli_idx:
-        value = sys.argv[cli_idx]
-    else:
-        value = os.getenv(env_name)
-
-    if value is not None:
-        return value
-
-    if default is not None:
-        return default
-
-    raise ArgNotProvidedException(cli_idx, env_name)
 
 
 def quit_handler(signum, frame):
@@ -50,11 +22,11 @@ def quit_handler(signum, frame):
 
 
 if __name__ == '__main__':
-    apiKey: str = read_arg(1, 'API_KEY')
-    period_sec: int = int(read_arg(2, 'PERIOD_SEC', -1))
-    redis_host: str = read_arg(3, 'REDIS_HOST', 'localhost')
-    redis_port: int = int(read_arg(4, 'REDIS_PORT', 6379))
-    redis_db: int = int(read_arg(5, 'REDIS_DB', 0))
+    apiKey: str = require_env(env('API_KEY'), 'API_KEY')
+    period_sec: int = int(env('PERIOD_SEC', -1))
+    redis_host: str = env('REDIS_HOST', 'localhost')
+    redis_port: int = int(env('REDIS_PORT', 6379))
+    redis_db: int = int(env('REDIS_DB', 0))
 
     place_id = CITY_ID_SOLNA
     url = CITY_CURRENT_WEATHER_URL.format(id=place_id, key=apiKey)
