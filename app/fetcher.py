d3050c902886211e05e1d19e43690466c16e3137
meandnano <meandnano@gmail.com>
2021-03-06 22:01:53 +0100

    Blacken fetcher.py

--- app/fetcher.py
+++ app/fetcher.py
@@ -11,22 +11,24 @@ from env_support import require_env
 
 CITY_ID_STHLM = 2673722
 CITY_ID_SOLNA = 2675397
-CITY_CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?" \
-                           "id={id}&appid={key}&units=metric"
+CITY_CURRENT_WEATHER_URL = (
+    "https://api.openweathermap.org/data/2.5/weather?"
+    "id={id}&appid={key}&units=metric"
+)
 
 
 def quit_handler(signum, frame):
     print()
-    print('Good bye')
+    print("Good bye")
     sys.exit(0)
 
 
-if __name__ == '__main__':
-    apiKey: str = require_env(env('API_KEY'), 'API_KEY')
-    period_sec: int = int(env('PERIOD_SEC', -1))
-    redis_host: str = env('REDIS_HOST', 'localhost')
-    redis_port: int = int(env('REDIS_PORT', 6379))
-    redis_db: int = int(env('REDIS_DB', 0))
+if __name__ == "__main__":
+    apiKey: str = require_env(env("API_KEY"), "API_KEY")
+    period_sec: int = int(env("PERIOD_SEC", -1))
+    redis_host: str = env("REDIS_HOST", "localhost")
+    redis_port: int = int(env("REDIS_PORT", 6379))
+    redis_db: int = int(env("REDIS_DB", 0))
 
     place_id = CITY_ID_SOLNA
     url = CITY_CURRENT_WEATHER_URL.format(id=place_id, key=apiKey)
@@ -41,7 +43,9 @@ if __name__ == '__main__':
     redis.ping()
 
     if period_sec > 0:
-        print(f"Fetching weather data every {period_sec} seconds. Use Ctrl+D to quit...\n")
+        print(
+            f"Fetching weather data every {period_sec} seconds. Use Ctrl+D to quit...\n"
+        )
 
     while True:
         resp = requests.get(url)
